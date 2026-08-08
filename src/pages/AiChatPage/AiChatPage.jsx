79ac988122cc1cafa5960417ab6be06cfa4a482f
luii0415 <becky45210193@gmail.com>
2026-08-08 17:19:00 +0900

    ✨ Feat : AI Chat 페이지 Route 추가
    ai chat route 추가

---
+++ src/pages/AiChatPage/AiChatPage.jsx
@@ -0,0 +1,12 @@
+import PagePlaceholder from "../../components/PagePlaceholder/PagePlaceholder";
+
+const AiChatPage = () => {
+  return (
+    <PagePlaceholder
+      description="AiChatPage이 표시될 화면입니다."
+      title="AiChatPage"
+    />
+  );
+};
+
+export default AiChatPage;
